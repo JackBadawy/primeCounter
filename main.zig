1e355a4249c40dd9dda8db4725320cb24237e8cc
JackBadawy <129567199+JackBadawy@users.noreply.github.com>
2024-08-20 06:33:17 +1000

    user input

--- main.zig
+++ main.zig
@@ -27,11 +27,44 @@ pub fn countPrimes(upTo: i32) !i32 {
     return count;
 }
 
+fn read_num(stdin: *const std.fs.File) i32 {
+    var buf: [20]u8 = undefined;
+    const len = stdin.*.read(&buf) catch |err| {
+        std.debug.print("Error while reading number: {}\n", .{err});
+        std.os.exit(1);
+    };
+
+    if (len == buf.len) {
+        std.debug.print("Input is too big!\n", .{});
+        std.os.exit(1);
+    }
+
+    const line = std.mem.trimRight(u8, buf[0..len], "\r\n");
+    const val = std.fmt.parseInt(i32, line, 10) catch |err| {
+        std.debug.print("Error while reading number: {}\n", .{err});
+        std.os.exit(1);
+    };
+
+    return val;
+}
+
 pub fn main() void {
-    const countOfPrimes: i32 = countPrimes(1000000) catch |err| {
+    const stdin = std.io.getStdIn();
+
+    std.debug.print("Enter a number to find the count of primes up to that number: ", .{});
+    var userInput: i32 = read_num(&stdin);
+
+    const start_time = std.time.milliTimestamp();
+    const countOfPrimes: i32 = countPrimes(userInput) catch |err| {
         std.debug.print("Error: {}\nNumber input must be larger than 1\n", .{err});
         return;
     };
+    const end_time = std.time.milliTimestamp();
+    const elapsed_time = end_time - start_time;
 
     std.debug.print("Count of primes: {}\n", .{countOfPrimes});
+    std.debug.print("Execution time: {} ms\n", .{elapsed_time});
+
+    std.debug.print("Press Enter to exit...\n", .{});
+    _ = read_num(&stdin);
 }
